48189b5d3b5073e54029fdca78af3c9e3c5c3801
Nukr <nukrs.w@gmail.com>
2016-09-07 10:31:49 +0800

    refactor a bit

--- main.go
+++ main.go
@@ -13,33 +13,84 @@ import (
 	"github.com/PuerkitoBio/goquery"
 )
 
-func main() {
-	if len(os.Args) < 2 {
-		fmt.Println("not enought arguments")
-		os.Exit(1)
+// Finder ...
+type Finder struct {
+	domain string
+	dir    string
+	urls   []string
+}
+
+func (f *Finder) download() {
+	createDirIfNotExists(f.dir)
+	for _, u := range f.urls {
+		downloadFile(u, f.dir)
 	}
-	ysURL := os.Args[1]
-	doc, err := goquery.NewDocument(ysURL)
+}
+
+func main() {
+	siteURL := checkArgs()
+	finder := newFinder(siteURL)
+	finder.download()
+}
+
+func newExtractor(domain string) func(u string) (string, []string) {
+	extractors := make(map[string]func(string) (string, []string))
+	extractors["www.16ys.org"] = extractor16ys
+	return extractors[domain]
+}
+
+func extractor16ys(u string) (string, []string) {
+	doc, err := goquery.NewDocument(u)
 	if err != nil {
 		log.Fatal(err)
 	}
 	r := strings.NewReplacer("/", "")
-	dirname := path.Join("/Users/nukr/Downloads", r.Replace(doc.Find("h4 a").Eq(0).Text()))
+	dir := path.Join("/Users/nukr/Downloads", r.Replace(doc.Find("h4 a").Eq(0).Text()))
+	var urls []string
 	doc.Find("#zoomtext .quote-content a").
 		Each(func(i int, s *goquery.Selection) {
 			href, _ := s.Attr("href")
 			idx := strings.Index(href, "fid=")
-			downloadFile("http://www.16ys.org/attachment.php?fid="+href[idx+4:], dirname)
+			urls = append(urls, "http://www.16ys.org/attachment.php?fid="+href[idx+4:])
 		})
+	return dir, urls
 }
 
-func downloadFile(u, dir string) {
+func newFinder(siteURL string) Finder {
+	domain := getDomain(siteURL)
+	dir, urls := newExtractor(domain)(siteURL)
+	finder := Finder{
+		domain: domain,
+		dir:    dir,
+		urls:   urls,
+	}
+	return finder
+}
+
+// TODO: implement real getDomain
+func getDomain(siteURL string) string {
+	return "www.16ys.org"
+}
+
+func checkArgs() string {
+	if len(os.Args) < 2 {
+		fmt.Println("not enought arguments")
+		os.Exit(1)
+	}
+	return os.Args[1]
+}
+
+// TODO: return error
+func createDirIfNotExists(dir string) {
 	if _, err := os.Stat(dir); os.IsNotExist(err) {
 		err := os.Mkdir(dir, 0755)
 		if err != nil {
 			log.Fatal(err)
 		}
 	}
+}
+
+func downloadFile(u, dir string) {
 	res, err := http.Get(u)
 	if err != nil {
 		log.Fatal(err)
---
+++ main_test.go
@@ -0,0 +1,7 @@
+package main
+
+import "testing"
+
+func TestDownload(t *testing.T) {
+	finder := newFinder("xx.xx.xx")
+}
